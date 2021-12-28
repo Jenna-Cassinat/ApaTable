541dceb7bf0f240b558e0f2acb395bdaba2a6a73
Jenna Dayley <A02253468@aggies.usu.edu>
2021-12-28 16:17:35 -0700

    Advance thing

--- R/texregBetter.R
+++ R/texregBetter.R
@@ -1,7 +1,16 @@
+# Be sure that your document includes \usepackage{multirow} !
+
 texregBetter <- function(
   l,
+  modelNames=sapply(1:length(l),function(i)paste("Model",i)),
+  includeStandardBeta=TRUE,
+  caption="Statistical models",
   ... # Additional parameters to matrixreg
 ){
+
+  # Validate modelNames
+  if(length(l)!=length(modelNames))
+    stop("Please supply a single name for each model.")
 
   # Run texreg for matrix data ----
   m <- texreg::matrixreg(
@@ -22,16 +31,17 @@ texregBetter <- function(
   # Count models ----
   nModels <- ncol(d)-1
 
+  # Count variables ----
+  nVar <- which(d[,1]=="nimp")-1
+
   # Rearrange data in dataframe for standard error and eventual standard beta ----
   for(rw in 1:nrow(d))
     for(cn in 1:ncol(d))
       if(!nchar(d[rw,cn]))
         d[rw,cn] <- NA
   prefixSe <- paste0(uuid::UUIDgenerate(),"_")
-  prefixSb <- paste0(uuid::UUIDgenerate(),"_")
   for(i in 1:nModels){
     d[[paste0(prefixSe,i)]] <- NA
-    d[[paste0(prefixSb,i)]] <- NA
   }
   for(rw in which(is.na(d[,1])))
     for(cn in 2:(nModels+1)){
@@ -55,19 +65,68 @@ texregBetter <- function(
     colOrder <- c(
       colOrder,
       colnames(d)[i+1],
-      paste0(prefixSe,i),
-      paste0(prefixSb,i)
+      paste0(prefixSe,i)
     )
   d <- d[,colOrder]
 
-  View(d) #TEMP
-  return(d) #TEMP
+  # Rename all columns ----
+  colnames(d) <- 1:ncol(d)
+
+  # Kablize table ----
+  final <- knitr::kable(
+    d,
+    "latex",
+    caption=caption
+  )
+
+  # Drop in custom header ----
+  headerPattern <- "(?<=\\\\hline\\n)[^\\\\\\\\]+\\\\\\\\"
+  customHeaderVarCell <- paste0("\\\\multirow{2}{*}{",variableColName,"} \\\\\\\\")
+  customHeaderModelNamesLine <- paste(
+    sapply(
+      modelNames,
+      function(x)
+        paste0("& \\\\multicolumn{2}{c}{",x,"}")
+    ),
+    collapse=" "
+  )
+  customHeaderModelNamesLine <- paste(
+    customHeaderModelNamesLine,
+    "\\\\\\\\"
+  )
+  customHeaderDetailsLine <- paste(
+    sapply(
+      1:nModels,
+      function(i)
+        "& $B$ & $SE$"
+    ),
+    collapse=" "
+  )
+  customHeaderDetailsLine <- paste(
+    customHeaderDetailsLine,
+    "\\\\\\\\"
+  )
+  customHeader <- paste(
+    customHeaderVarCell,
+    customHeaderModelNamesLine,
+    paste0("\\\\cline{2-",ncol(d),"}"),
+    customHeaderDetailsLine,
+    collapse="\n"
+  )
+  final <- sub(headerPattern,customHeader,final,perl = TRUE)
+
+  # Remove all vertical lines ----
+  beginTab <- stringr::str_match(
+    final,
+    "\\\\begin\\{tabular\\}(\\[[A-z]\\])?\\{([A-z]\\|)+[A-z]\\}"
+  )[1,1]
+  beginTabNoPipes <- gsub("|","",beginTab,fixed = TRUE)
+  final <- sub(beginTab,beginTabNoPipes,final,fixed = TRUE)
 
   # Cat final ----
   cat(final)
 
 }
-
 
 # TEST #TEMP ----
 
