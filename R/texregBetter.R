37a6641b62f0a9294af507192aacb8f5da6c6e8e
Jon Dayley <jonathandayley@gmail.com>
2022-01-01 09:35:27 -0700

    Remove test from source

--- R/texregBetter.R
+++ R/texregBetter.R
@@ -226,39 +226,3 @@ texregBetter <- function(
   cat(final)
 
 }
-
-# TEST #TEMP ----
-
-walkingImp <- mice::mice(
-  mice::walking,
-  m=5,
-  maxit=5,
-  meth='pmm',
-  print=FALSE
-)
-m1 <- with(walkingImp,lm(walkingImp$data$age~walkingImp$data$sex+walkingImp$data$YA))
-m2 <- with(walkingImp,lm(walkingImp$data$age~walkingImp$data$sex+walkingImp$data$YB))
-mList <- list(
-  m1,
-  m2
-)
-coefNamesSquare <- sapply(
-  mList,
-  function(m){
-    sapply(
-      1:length(m$analyses),
-      function(i)
-        names(m$analyses[[i]]$coefficients)
-    )
-  }
-)
-coefNames <- c()
-for(i in 1:ncol(coefNamesSquare))
-  coefNames <- c(
-    coefNames,
-    coefNamesSquare[,i]
-  )
-coefNames <- gsub("^.+\\$","",unique(coefNames))
-texregBetter(
-  l = lapply(mList,mice::pool)
-)
